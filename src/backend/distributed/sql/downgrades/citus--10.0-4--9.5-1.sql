9bbed4368c6b31a7cd5bb5aef737330ccec8064f
Hanefi Onaldi <Hanefi.Onaldi@microsoft.com>
2021-06-08 20:54:54 +0300

    Remove paths to 10.0-1 and introduce 10.0-4 Changes to be committed: 	created:    citus--9.5-1--10.0-4.sql 	deleted:    citus--9.5-1--10.0-1.sql 	deleted:    citus--10.0-1--10.0-2.sql 	deleted:    citus--10.0-2--10.0-3.sql
    	created:    downgrades/citus--10.0-4--9.5-1.sql
    	deleted:    downgrades/citus--10.0-3--10.0-2.sql
    	deleted:    downgrades/citus--10.0-2--10.0-1.sql
    	deleted:    downgrades/citus--10.0-1--9.5-1.sql
    
    The commands I ran were as follows:
    - in src/backend/distributed/sql directory:
    	- cat citus--9.5-1--10.0-1.sql citus--10.0-1--10.0-2.sql citus--10.0-2--10.0-3.sql > citus--9.5-1--10.0-4.sql
    	- rm -f  citus--9.5-1--10.0-1.sql citus--10.0-1--10.0-2.sql citus--10.0-2--10.0-3.sql
    - in src/backend/distributed/sql/downgrades directory:
    	- cat citus--10.0-3--10.0-2.sql citus--10.0-2--10.0-1.sql citus--10.0-1--9.5-1.sql > citus--10.0-4--9.5-1.sql
    	- rm -f citus--10.0-3--10.0-2.sql citus--10.0-2--10.0-1.sql citus--10.0-1--9.5-1.sql
    
    Add missing changes
    Changes to be committed:

--- src/backend/distributed/sql/downgrades/citus--10.0-4--9.5-1.sql
+++ src/backend/distributed/sql/downgrades/citus--10.0-4--9.5-1.sql
@@ -1,3 +1,33 @@
+-- citus--10.0-3--10.0-2
+-- this is a downgrade path that will revert the changes made in citus--10.0-2--10.0-3.sql
+
+DROP FUNCTION pg_catalog.citus_update_table_statistics(regclass);
+
+#include "../udfs/citus_update_table_statistics/10.0-1.sql"
+
+CREATE OR REPLACE FUNCTION master_update_table_statistics(relation regclass)
+RETURNS VOID AS $$
+DECLARE
+	colocated_tables regclass[];
+BEGIN
+	SELECT get_colocated_table_array(relation) INTO colocated_tables;
+
+	PERFORM
+		master_update_shard_statistics(shardid)
+	FROM
+		pg_dist_shard
+	WHERE
+		logicalrelid = ANY (colocated_tables);
+END;
+$$ LANGUAGE 'plpgsql';
+COMMENT ON FUNCTION master_update_table_statistics(regclass)
+	IS 'updates shard statistics of the given table and its colocated tables';
+
+DROP FUNCTION pg_catalog.citus_get_active_worker_nodes(OUT text, OUT bigint);
+/* citus--10.0-2--10.0-1.sql */
+#include "../../../columnar/sql/downgrades/columnar--10.0-2--10.0-1.sql"
+
+REVOKE SELECT ON public.citus_tables FROM public;
 -- citus--10.0-1--9.5-1
 
 -- In Citus 10.0, we added another internal udf (notify_constraint_dropped)
